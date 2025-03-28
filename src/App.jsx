b16f1fbb216be0558e7a18024791251a7035c6c4
Shahriar Fardows <shahriarfardows@gmail.com>
2025-03-28 22:36:35 +0600

    calander update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { AppProvider, Button, Card, FormLayout, Text, TextField } from "@shopify/polaris";
+import {  useCallback, useState } from "react";
+import { AppProvider, Button, Card, DatePicker, FormLayout, Text, TextField } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
 import TaskLists from "./Component/TaskLists";
 
@@ -12,26 +12,32 @@ function App() {
 
   const [todos, setTodos] = useState(todoList);
   const [newTodo, setNewTodo] = useState('');
+  const [selectedDates, setSelectedDates] = useState(new Date());
+  const [{month, year}, setDate] = useState({month: 1, year: 2018});
 
+  const handleMonthChange = useCallback(
+    (month, year) => setDate({month, year}),
+    [],
+  );
 
 const addTask = (e) => {
     e.preventDefault();
-    const date = e.target.dueDate.value; 
+    // const date = e.target.dueDate.value; 
 
-    if (!newTodo || !date) return; 
+    if (!newTodo || !selectedDates) return; 
 
     const newTask = {
       id: todos.length + 1, 
       text: newTodo,
       completed: false,
-      dueDate: new Date(date), 
+      dueDate: selectedDates.start,
     };
 
     console.log("New Task: ", newTask); 
 
     setTodos([...todos, newTask]); 
     setNewTodo(''); 
-    e.target.dueDate.value = '';
+    // e.target.dueDate.value = '';
   };
 
   return (
@@ -52,19 +58,13 @@ const addTask = (e) => {
                 <form onSubmit={addTask} className="p-5">
                   <FormLayout>
                     <TextField label="Task description" value={newTodo} onChange={setNewTodo} autoComplete="off"/>
-                    <div className="relative my-6">
-                      <input
-                        id="id-date07"
-                        type="date"
-                        name="dueDate"
-                        className="peer relative h-10 w-full rounded border border-slate-200 px-4 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-black focus:outline-none invalid:focus:border-pink-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
-                      />
-                      <label
-                        className="absolute -top-2 left-2 z-[1] cursor-text px-2 text-xs text-slate-400 transition-all before:absolute before:left-0 before:top-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-autofill:-top-2 peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:cursor-default peer-focus:text-xs peer-focus:text-black peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
-                      >
-                        Due Date
-                      </label>
-                    </div>
+                    <DatePicker
+              month={month}
+              year={year}
+              onChange={setSelectedDates}
+              onMonthChange={handleMonthChange}
+              selected={selectedDates}
+            />
 
                     <Button submit primary>Add Task</Button>
                   </FormLayout>
